23fc0419d5a0925257c3e365976960d568fe34fa
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-07-20 02:22:16 +0900

    2. 객체 지향 원리 적용 - 관심사의 분리 fin

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,6 +1,8 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import hello.core.member.Grade;
@@ -9,8 +11,17 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
